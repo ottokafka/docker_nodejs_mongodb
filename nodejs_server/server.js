8be9e60f4595243f0896f0d1721a9401b7cfc08a
otto <otto@elfo.com>
2020-10-18 22:50:21 +0800

    edit dockerfiles

--- nodejs_server/server.js
+++ nodejs_server/server.js
@@ -5,15 +5,15 @@ const mysql = require('mysql');
 
 const PORT = 4000;
 
-// // USE THIS BECAUSE of based of name we gave the database
-// mongoose.connect('mongodb://database_mongodb:27017/mydb').then(() => console.log("connected to mongodb")).catch((err) => console.log(err))
+// USE THIS BECAUSE of based of name we gave the database
+mongoose.connect('mongodb://database_mongodb:27017/mydb').then(() => console.log("connected to mongodb")).catch((err) => console.log(err))
 
-// // Connect to mySQL database
-// mysql.createConnection({
-//     host: "database_mysql",
-//     user: "root",
-//     password: "root"
-// }).connect(() => console.log("MySQL connected"))
+// Connect to mySQL database
+mysql.createConnection({
+    host: "database_mysql",
+    user: "root",
+    password: "root"
+}).connect(() => console.log("MySQL connected"))
 
 // Gives user a response when visiting localhost:4000
 app.get('/', function (req, res) {
