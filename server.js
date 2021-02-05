8eb418d1b9915287308a8d1537fe4ecfd32898a1
otto <otto@elfo.com>
2021-02-05 11:06:00 +0000

    serverjs mongodb connection fix

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 const PORT = 4000;
 
 // USE THIS BECAUSE of based of name we gave the database
-mongoose.connect('mongodb://database:27017/mydb').then(() => console.log("connected to mongodb")).catch((err) => console.log(err))
+mongoose.connect('mongodb://database_mongodb:27017/mydb').then(() => console.log("connected to mongodb")).catch((err) => console.log(err))
 
 // Gives user a response when visiting localhost:4000
 app.get('/', function (req, res) {
